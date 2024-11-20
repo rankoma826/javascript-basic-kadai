9134454f8d44835701a8e171db8b1758c6efde86
rankoma826 <ftpj.0802@gmail.com>
2024-11-20 15:26:52 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -5,6 +5,7 @@ let Weight;
 Height = '身長';
 Weight = '体重';
 
+
 let bmi = '体重/(身長＊身長)'
 
 
@@ -13,6 +14,6 @@ console.log(bmi);
 Height = 1.7;
 Weight = 68;
 
-console.log(68/(1.7*1.7))
+console.log(Weight/(Height*Height))
 
 
